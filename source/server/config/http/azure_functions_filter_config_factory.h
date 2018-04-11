8897ccd18ebd8488f4d5ce56666f0d9a55c553ca
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-04-11 18:06:56 -0400

    Upgrade Envoy (#4)
    1. Upgrade Envoy.
    2. Reduce dependencies in BUILD file to compile tests faster.

--- source/server/config/http/azure_functions_filter_config_factory.h
+++ source/server/config/http/azure_functions_filter_config_factory.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include "server/config/http/empty_http_filter_config.h"
+#include "extensions/filters/http/common/empty_http_filter_config.h"
 
 namespace Envoy {
 namespace Server {
 namespace Configuration {
 
-class AzureFunctionsFilterConfigFactory : public EmptyHttpFilterConfig {
+class AzureFunctionsFilterConfigFactory
+    : public Extensions::HttpFilters::Common::EmptyHttpFilterConfig {
 public:
   // Server::Configuration::NamedHttpFilterConfigFactory
   std::string name() override;
